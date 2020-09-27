2004e14acb570edbc8291f2296a73caeab238d32
Nimmi <nimmisumith@gmail.com>
2020-09-27 20:37:14 +0530

    Initial Commit

---
+++ IOSExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSExercise
+//
+//  Created by Nimmi P on 27/09/20.
+//  Copyright © 2020 Nimmi P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
